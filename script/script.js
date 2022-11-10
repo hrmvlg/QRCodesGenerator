9ad78ad1b3b16a99f623239f5293d9f0151f8867
OlyaKhromova <olya.hrmvlg@gmail.com>
2022-11-10 22:07:06 +0300

    Create Save Button + get QR Code image source + clear button from UI

--- script/script.js
+++ script/script.js
@@ -4,7 +4,7 @@ const qr = document.getElementById('qrcode');
 const onGenerateSubmit = (e) => {
     e.preventDefault();
     clearUI();
-    
+
     const url = document.getElementById('url').value;
     const size = document.getElementById('size').value;
 
@@ -16,6 +16,11 @@ const onGenerateSubmit = (e) => {
         setTimeout(() => {
             hideSpinner();
             generateQRCode(url, size);
+            
+            setTimeout(()=>{
+                const saveUrl = qr.querySelector('img').src;
+                createSaveBtn(saveUrl);
+            }, 50)
         }, 1000);
     }
 };
@@ -38,7 +43,19 @@ const hideSpinner = () => {
 
 const clearUI = () => {
     qr.innerHTML = '';
+    const saveLink = document.getElementById('save-link');
+    if(saveLink) saveLink.remove();
 }
+
+const createSaveBtn = (saveUrl) => {
+    const link = document.createElement('a');
+    link.id = 'save-link';
+    link.classList = 'bg-red-500 hover:bg-red-700 text-white font-bold py-2 rounded w-1/3 m-auto my-5';
+    link.href = saveUrl;
+    link.download = 'qrcode';
+    link.innerHTML = 'Save Image';
+    document.getElementById('generated').appendChild(link);
+};
 
 hideSpinner();
 form.addEventListener('submit', onGenerateSubmit);
